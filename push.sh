3e2bbea52ad761026bc46f2c6612e1d450e81aeb
Byron Marohn <Combustible@live.com>
2021-03-07 20:10:35 +0000

    Add additional scripts
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- push.sh
+++ push.sh
@@ -5,12 +5,12 @@ sh compile.sh
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR/target"
 
-plugin="$(ls -r FRED_*.jar | grep -v sources | head -n 1)"
+plugin="$(ls -r Roguelite*.jar | grep -v sources | head -n 1)"
 if [[ -z "$plugin" ]]; then
 	exit 1
 fi
 
 echo "Plugin version: $plugin"
 
-ssh -p 8822 epic@build.playmonumenta.com "cd /home/epic/dev1_shard_plugins && rm -f FRED_*.jar"
+ssh -p 8822 epic@build.playmonumenta.com "cd /home/epic/dev1_shard_plugins && rm -f Roguelite*.jar"
 scp -P 8822 $plugin epic@build.playmonumenta.com:/home/epic/dev1_shard_plugins/
