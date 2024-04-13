367209325928db038a2a2b20c5db1d9fc71feb10
Timothy Southwick <NickNackGus@gmail.com>
2024-04-13 15:52:24 -0400

    Added script to deploy to volt

---
+++ push_volt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+./compile.sh
+cd "$SCRIPT_DIR/target"
+
+plugin="$(ls -r Roguelite*.jar | grep -v sources | head -n 1)"
+if [[ -z "$plugin" ]]; then
+	exit 1
+fi
+
+echo "Plugin version: $plugin"
+
+scp -P 9922 $plugin epic@admin.playmonumenta.com:/home/epic/volt/m12/server_config/plugins
+ssh -p 9922 epic@admin.playmonumenta.com "cd /home/epic/volt/m12/server_config/plugins && rm -f Roguelite.jar ; ln -s $plugin Roguelite.jar"
+ssh -p 9922 epic@admin.playmonumenta.com "cd /home/epic/volt/m13/server_config/plugins && rm -f Roguelite.jar ; ln -s $plugin Roguelite.jar"
